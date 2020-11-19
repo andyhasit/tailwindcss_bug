adb722d2c7a3981d66c5da70924acff63ca492d8
Andrew Buchan <andyhasit@gmail.com>
2020-11-19 22:09:11 +0000

    start

---
+++ check.sh
@@ -0,0 +1,1 @@
+grep -B 5 -A 5 $1 ./out.css
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    content:  [
+      'my-code',
+    ],
+    options: {
+      safelist: ['hidden', 'text-red-200'],
+    }
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
+
